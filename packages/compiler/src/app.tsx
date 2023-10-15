9067a600ec049efa594076993d612208fc138161
Handsome WuWu <1042729119@qq.com>
2023-10-15 20:25:18 +0800

    引入umi重构项目

--- packages/compiler/src/app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-const App: React.FC = () => {
-    return (
-        <div>我是编译器</div>
-    )
-}
-
-export default App
--- packages/editor/src/app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-const App: React.FC = () => {
-    return (
-        <div>我是编辑器</div>
-    )
-}
-
-export default App
